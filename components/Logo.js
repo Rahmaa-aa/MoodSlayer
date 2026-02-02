3e1a5d958f180dbe752e8bdcd8c25c23ede0dba7
Rahmaa-aa <rahma.ayed@esprit.tn>
2026-02-02 12:40:39 +0100

    fix: use correct logo filename in Logo component

--- components/Logo.js
+++ components/Logo.js
@@ -4,7 +4,7 @@ export function Logo({ width = 200, height = 200, className = "" }) {
     return (
         <div className={`logo-container ${className}`} style={{ position: 'relative', width: `${width}px`, height: `${height}px` }}>
             <Image
-                src="/logo.png"
+                src="/logo mood slayer.png"
                 alt="MoodSlayer Logo"
                 fill
                 priority
